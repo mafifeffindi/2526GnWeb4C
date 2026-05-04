01ece3f1aad35e15840b607645f3d0b2b2f693ea
inayahmasruro-debug <inayahmasruro@gmail.com>
2026-05-05 03:00:33 +0700

    Create eksperimen.php

---
+++ Pekan 09/098_Sulis Inayah/eksperimen.php
@@ -0,0 +1,19 @@
+<?php
+echo "<h1>Profil Programmer</h1>";
+echo "Nama         : Sulis Inayah Masruro";
+echo "<br>";
+
+echo "Universitas  : UTM";
+echo "<br>";
+
+echo "Hobi         : menyanyi";
+echo "<br>";
+
+echo "Cita-cita    : orang sukses";
+echo "<br>";
+
+echo "Alasan belajar web programming : seru";
+echo "<br>";
+
+echo "<p>Program ini dibuat untuk memenuhi tugas Modul 9 - PHP Hypertext Preprocessor</p>";
+?>
