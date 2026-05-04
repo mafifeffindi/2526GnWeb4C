57dbac7825165fdb5c7e879812998cb1869dd45d
MUHAMMAD EDRIC FAUSTADANENDRA <edrikfausta29@gmail.com>
2026-05-04 21:59:30 +0700

    Add files via upload

---
+++ Pekan 09/084_Muhammad Edric Faustadanendra/eksperimen.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eksperimen PHP</title>
+</head>
+<body>
+
+<h1>Profil Mahasiswa</h1>
+<hr>
+
+<?php
+echo "Nama: Muhammad Edric Faustadanendra";
+echo "<br>";
+
+echo "Universitas: Universitas Trunojoyo Madura";
+echo "<br>";
+
+echo "Hobi: Ngoding & Gaming";
+echo "<br>";
+
+echo "Cita-cita: Software Developer";
+echo "<br>";
+
+echo "Alasan belajar web programming: Ingin membuat website dan aplikasi modern";
+echo "<br>";
+?>
+
+</body>
+</html>
---
+++ Pekan 09/084_Muhammad Edric Faustadanendra/halaman.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman PHP</title>
+</head>
+<body>
+
+<h1>Belajar PHP Dasar</h1>
+
+<p>Ini adalah contoh kombinasi HTML dan PHP dalam satu halaman.</p>
+
+<?php
+echo "<b>Hari ini saya belajar PHP</b><br>";
+echo "<i>PHP berjalan di server</i><br>";
+echo "<u>Saya ingin menjadi programmer handal</u><br>";
+?>
+
+</body>
+</html>
