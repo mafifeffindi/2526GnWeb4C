ca8f6931912c948a3356b14cecb6e41f20c1f7ed
075_Binti Nur Hamidah <bintihamida7@gmail.com>
2026-05-04 22:26:30 +0700

    Add files via upload

---
+++ Pekan 09/075_Binti Nur Hamidah/eksperimen.php
@@ -0,0 +1,33 @@
+<?php
+
+echo "<body style='background-color:#d6e6ff; font-family:Arial;'>";
+
+echo "<h2 style='text-align:center; color:#333;'>Profil Programmer</h2>";
+echo "<hr style='width:50%;'>";
+
+echo "<div style='
+    width:50%;
+    margin:auto;
+    background-color:#ffd6e0;
+    padding:20px;
+    border-radius:10px;
+    box-shadow:0 0 10px rgb(249, 188, 233);
+    line-height:1.8;
+'>";
+
+echo "<p><b>Nama:</b> Binti Nur Hamidah</p>";
+echo "<p><b>NIM:</b> 240631100075</p>";
+echo "<p><b>Prodi:</b> Pendidikan Informatika</p>";
+echo "<p><b>Fakultas:</b> Keguruan dan Ilmu Pendidikan</p>";
+echo "<p><b>Universitas:</b> Universitas Trunojoyo Madura</p>";
+echo "<p><b>Hobi:</b> Membaca novel dan menonton</p>";
+echo "<p><b>Cita-cita:</b> Guru</p>";
+echo "<p><b>Alasan belajar web programming:</b> Ingin lebih menguasai bidang IT</p>";
+echo "<p><b>Bahasa favorit:</b> PHP</p>";
+echo "<p><b>Tahun belajar:</b> 2026</p>";
+
+echo "</div>";
+
+echo "</body>";
+
+?>
---
+++ Pekan 09/075_Binti Nur Hamidah/halaman.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman PHP</title>
+</head>
+
+<body style="
+    font-family:'Trebuchet MS', 'Comic Sans MS', sans-serif;
+    background: linear-gradient(to right, #ffd6e0, #d6e6ff);
+">
+
+    <h1 style="
+        text-align:center;
+        color:#6b5b95;
+        margin-top:30px;
+    ">
+        ✨Belajar PHP✨ 
+    </h1>
+
+    <p style="text-align:center; color:#555;">
+        Ini adalah contoh sederhana kombinasi HTML dan PHP.
+    </p>
+
+    <div style="
+        width:50%;
+        margin:30px auto;
+        background-color:#ffffffcc;
+        padding:20px;
+        border-radius:12px;
+        box-shadow:0 4px 10px rgba(0,0,0,0.1);
+    ">
+
+        <hr style="border:1px solid #f3c4d3;">
+
+        <?php
+        echo "<p><b>Nama:</b> Binti Nur Hamidah</p>";
+        echo "<p><b>NIM:</b> 240631100075</p>";
+        echo "<p><b>Prodi:</b> Pendidikan Informatika</p>";
+        echo "<p><b>Belajar:</b> Server Side Programming</p>";
+        ?>
+
+        <hr style="border:1px solid #c4d7f2;">
+
+    </div>
+
+    <p style="text-align:center; color:#555;">
+        Mari belajar PHP dan HTML bersama - sama!!
+    </p>
+
+    <p style="text-align:center; color:#6b5b95;">
+         ©hamidah2026 
+    </p>
+
+</body>
+</html>
