1f93d2610afd866a56759d351f2f19ed2686cd5f
inayahmasruro-debug <inayahmasruro@gmail.com>
2026-05-05 03:12:09 +0700

    Add files via upload

---
+++ Pekan 09/098_Sulis Inayah/halaman.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan PHP</title>
+</head>
+<body>
+
+<h1>Gabungan HTML & PHP</h1>
+<hr>
+
+<?php
+
+echo "Nama         : Sulis Inayah Masruro";
+echo "<br>";
+
+echo "Program Studi: Pendidikan Informatika";
+echo "<br>";
+
+echo "Tahun        : 2024";
+echo "<br>";
+
+echo "Mata Kuliah  : PEMROGRAMAN WEB";
+echo "<br>";
+
+echo "kalimat       : HALOOOO!";
+echo "<br>";
+?>
+
+<hr>
+</body>
+</html>
