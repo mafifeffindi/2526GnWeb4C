893ad6af15c5a7b45796a412a7041820a494f47f
silfiyatulfikriyasilfi-droid <silfiyatulfikriyasilfi@gmail.com>
2026-05-04 18:31:14 +0700

    Add files via upload

---
+++ pekan 9/Silfiya088/eksperimen.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Eksperimen PHP</title>
+</head>
+<body>
+
+<h1>Eksperimen PHP Saya</h1>
+
+<p>
+    Pada praktikum ini, saya belajar bagaimana PHP bekerja di sisi server. 
+    PHP dijalankan oleh server, bukan oleh browser. Hasilnya dikirim 
+    ke browser dalam bentuk HTML.
+</p>
+
+<?php
+    echo "Nama: Silfiya";
+    echo "<br>";
+    echo "Universitas: Universitas Trunodjoyo Madura";
+    echo "<br>";
+    echo "Hobi: Mendengarkan musik dan berimajinasi";
+    echo "<br>";
+    echo "Cita-cita: Guru";
+    echo "<br>";
+    echo "alasan belajar web programming: agar lebih paham bagaimana cara buat web dengan mudah tanpa database";
+?>
+
+</body>
+</html>
