29aec4f9a3e6baafaddcc9a1581a59f8962cdd12
muyassaroh1903-dev <muyassaroh1903@gmail.com>
2026-05-05 01:16:29 +0700

    Add files via upload

---
+++ Pekan 09/halaman.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Halaman PHP</title>
+</head>
+
+<body>
+
+    <h1>Belajar HTML dan PHP</h1>
+
+    <p>
+        PHP adalah bahasa pemrograman server-side untuk membuat website dinamis. PHP digunakan dengan membuat file .php, menulis kode di dalam tag <?php ?>, lalu menjalankannya melalui web server seperti XAMPP.
+    </p>
+
+    <?php
+
+    echo "Nama : Muyas";
+    echo "<br>";
+
+    echo "Kelas : 4C";
+    echo "<br>";
+
+    echo "Sedang belajar : Web Programming";
+
+    ?>
+
+</body>
+
+</html>
