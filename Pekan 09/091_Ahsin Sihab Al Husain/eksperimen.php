8ca1dd26b5715e2affa854764ca4069855ae1132
MUHAMMAD EDRIC FAUSTADANENDRA <edrikfausta29@gmail.com>
2026-05-05 08:31:45 +0700

    Add files via upload

---
+++ Pekan 09/091_Ahsin Sihab Al Husain/eksperimen.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Programmer</title>
+</head>
+<body>
+
+<?php
+// Judul menggunakan tag HTML (H1)
+echo "<h1>Profil Programmer</h1>";
+
+// Garis pemisah
+echo "<hr>";
+
+// Informasi minimal 5 baris (7 baris disediakan)
+echo "Nama : Ahsin Sihab Al Husain<br>";
+echo "Bahasa favorit : Python<br>";
+echo "Tahun belajar : 2024<br>";
+echo "Universitas : Universitas Trunojdoyo Madura<br>";
+echo "Hobi : Bermain Game dan Futsal<br>";
+echo "Cita-cita : Dosen<br>";
+echo "Alasan belajar web programming : Karena ingin jadi developer sebuah game online<br>";
+?>
+
+</body>
+</html>
