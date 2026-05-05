bc17e8fe99ab01779ae949fd01f65c91c2ca000c
240631100073-tech <240631100073@student.trunojoyo.ac.id>
2026-05-05 10:29:03 +0700

    Add PHP code to display student profile information

--- Pekan 09/073_Tsaqif Ahsanudiin/eksperimen.php
+++ Pekan 09/073_Tsaqif Ahsanudiin/eksperimen.php
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Profil Programmer - Modifikasi</title>
+</head>
+<body>
+
+    <h1>Profil Mahasiswa</h1>
+
+    <hr>
+
+    <?php
+        // 3. Menampilkan minimal 5 baris informasi menggunakan PHP
+        echo "Nama: <b>Andi</b><br>";
+        echo "Universitas: <b>Universitas Gajah Mada</b><br>";
+        echo "Hobi: <b>Koding dan Membaca</b><br>";
+        echo "Cita-cita: <b>Fullstack Developer</b><br>";
+        echo "Alasan Belajar Web: <b>Ingin membangun platform digital yang bermanfaat bagi orang banyak.</b><br>";
+    ?>
+
+</body>
+</html>
 
