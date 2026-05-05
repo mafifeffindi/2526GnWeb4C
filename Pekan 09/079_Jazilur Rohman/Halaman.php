5b740d418a6cff32b994cb577be98693d978fc31
jazilur033-ops <jazilur033@gmail.com>
2026-05-05 08:15:23 +0700

    Update Halaman.php

--- Pekan 09/079_Jazilur Rohman/Halaman.php
+++ Pekan 09/079_Jazilur Rohman/Halaman.php
@@ -1,1 +1,21 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <title>Latihan PHP</title>
+</head>
+<body>
 
+<h1>Belajar PHP</h1>
+
+<p>Ini adalah paragraf pertama dalam latihan PHP. Server side programming memungkinkan kita membuat halaman web dinamis.</p>
+
+<?php
+echo "Hari ini saya belajar server side programming.<br>";
+
+echo "PHP adalah bahasa pemrograman yang populer untuk web development.<br>";
+
+echo "Saya senang bisa mempelajari PHP lebih dalam.";
+?>
+
+</body>
+</html>
