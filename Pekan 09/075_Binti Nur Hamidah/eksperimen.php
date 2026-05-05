eef957b2ce3fb8249566e7d0bee49c231b153551
075_Binti Nur Hamidah <bintihamida7@gmail.com>
2026-05-05 07:29:55 +0700

    Add files via upload

--- Pekan 09/075_Binti Nur Hamidah/eksperimen.php
+++ Pekan 09/075_Binti Nur Hamidah/eksperimen.php
@@ -1,33 +1,17 @@
 <?php
 
-echo "<body style='background-color:#d6e6ff; font-family:Arial;'>";
+echo "Profil Programmer<br>";
+echo "-------------------------<br><br>";
 
-echo "<h2 style='text-align:center; color:#333;'>Profil Programmer</h2>";
-echo "<hr style='width:50%;'>";
-
-echo "<div style='
-    width:50%;
-    margin:auto;
-    background-color:#ffd6e0;
-    padding:20px;
-    border-radius:10px;
-    box-shadow:0 0 10px rgb(249, 188, 233);
-    line-height:1.8;
-'>";
-
-echo "<p><b>Nama:</b> Binti Nur Hamidah</p>";
-echo "<p><b>NIM:</b> 240631100075</p>";
-echo "<p><b>Prodi:</b> Pendidikan Informatika</p>";
-echo "<p><b>Fakultas:</b> Keguruan dan Ilmu Pendidikan</p>";
-echo "<p><b>Universitas:</b> Universitas Trunojoyo Madura</p>";
-echo "<p><b>Hobi:</b> Membaca novel dan menonton</p>";
-echo "<p><b>Cita-cita:</b> Guru</p>";
-echo "<p><b>Alasan belajar web programming:</b> Ingin lebih menguasai bidang IT</p>";
-echo "<p><b>Bahasa favorit:</b> PHP</p>";
-echo "<p><b>Tahun belajar:</b> 2026</p>";
-
-echo "</div>";
-
-echo "</body>";
+echo "Nama : Binti Nur Hamidah<br>";
+echo "NIM : 240631100075<br>";
+echo "Prodi : Pendidikan Informatika<br>";
+echo "Fakultas : Keguruan dan Ilmu Pendidikan<br>";
+echo "Universitas : Universitas Trunojoyo Madura<br>";
+echo "Hobi : Membaca novel dan menonton<br>";
+echo "Cita-cita : Guru<br>";
+echo "Alasan belajar web programming : Ingin lebih menguasai bidang IT<br>";
+echo "Bahasa favorit : PHP<br>";
+echo "Tahun belajar : 2026<br>";
 
 ?>
