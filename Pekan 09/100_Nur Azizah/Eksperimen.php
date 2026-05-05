9f3b711cfe65cc420be82e285252bf796e6a19a2
nurazizah-beep <nurazizah30705@gmail.com>
2026-05-05 08:20:05 +0700

    Add files via upload

---
+++ Pekan 09/100_Nur Azizah/Eksperimen.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "<h2>Profil Mahasiswa 4C</h2>";
+
+echo "<hr>";
+
+
+echo "Nama: Nur Azizah";
+echo"<br>";
+echo "Universitas: Universitas Trunodjoyo Madura";
+echo"<br>";
+echo "Hobi: Menggambar";
+echo"<br>";
+echo "Cita-cita: Menjadi menjadi pacar seonghyeon";
+echo"<br>";
+echo "Alasan belajar web programming: Agar bisa membangun aplikasi yang bermanfaat bagi banyak orang.";
+?>
---
+++ Pekan 09/100_Nur Azizah/halaman.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Latihan PHP - Aktivitas 4</title>
+</head>
+<body>
+
+    <!-- 1 Judul HTML -->
+    <h1>Dashboard Belajar Pemrograman Web</h1>
+
+    <!-- 1 Paragraf HTML -->
+    <p>Selamat datang di sesi praktik Modul 9. Di bawah ini adalah data yang dihasilkan melalui skrip server-side PHP:</p>
+
+    <hr>
+
+    <?php
+    // 3 Baris output PHP
+    echo "<strong>Output 1:</strong> Hari ini saya sedang belajar integrasi HTML dan PHP.<br>";
+    echo "<strong>Output 2:</strong> Server Apache sedang memproses kode ini menjadi tampilan web.<br>";
+    echo "<strong>Output 3:</strong> Status Pembelajaran: Aktif dan Sedang Berjalan.";
+    ?>
+
+</body>
+</html>
