da46a497f21fab5b29f27cf6e2630bf8110a5239
izulwafa123 <izulwafaabdulloh@gmail.com>
2026-05-05 08:45:38 +0700

    Create halaman.php

---
+++ Pekan 09/107_izul wafa abdulloh/eksperimen/halaman.php
@@ -0,0 +1,28 @@
+<?php
+// File: eksperimen.php
+// Nama: Izul Wafa Abdulloh
+// NIM: 107
+?>
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Saya</title>
+</head>
+<body>
+
+    <h1>Profil Mahasiswa</h1>
+    <hr>
+
+    <?php
+        echo "<p><strong>Nama:</strong> Izul Wafa Abdulloh</p>";
+        echo "<p><strong>NIM:</strong> 107</p>";
+        echo "<p><strong>Universitas:</strong> Universitas Trunojoyo Madura</p>";
+        echo "<p><strong>Hobi:</strong> Mendengarkan musik dan membaca novel</p>";
+        echo "<p><strong>Cita-cita:</strong> Menjadi Web Developer profesional</p>";
+        echo "<p><strong>Alasan Belajar Web Programming:</strong> Ingin bisa membuat website sendiri dan membuka peluang karir di bidang teknologi</p>";
+    ?>
+
+</body>
+</html>
