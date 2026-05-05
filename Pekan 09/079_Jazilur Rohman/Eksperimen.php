31b3b462746b26b9b2c7c8b95a9b0f13ceaf2990
jazilur033-ops <jazilur033@gmail.com>
2026-05-05 08:14:08 +0700

    Update Eksperimen.php

--- Pekan 09/079_Jazilur Rohman/Eksperimen.php
+++ Pekan 09/079_Jazilur Rohman/Eksperimen.php
@@ -1,1 +1,28 @@
 
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Programmer</title>
+</head>
+<body>
+
+<?php
+// Judul menggunakan tag HTML (H1)
+echo "<h1>Profil Programmer</h1>";
+
+// Garis pemisah
+echo "<hr>";
+
+// Informasi minimal 5 baris (7 baris disediakan)
+echo "Nama : Jazilur<br>";
+echo "Bahasa favorit : PHP<br>";
+echo "Tahun belajar : 2026<br>";
+echo "Universitas : Universitas Trunojoyo Madura<br>";
+echo "Hobi : Muncak<br>";
+echo "Cita-cita : web Developer<br>";
+echo "Alasan belajar web programming : Membangun aplikasi yang bermanfaat bagi banyak orang<br>";
+?>
+
+</body>
+</html>
