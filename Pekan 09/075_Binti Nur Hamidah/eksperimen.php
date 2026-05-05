8a488578efbe5891e71a9ef637a91e5f3388c6b7
075_Binti Nur Hamidah <bintihamida7@gmail.com>
2026-05-05 08:52:34 +0700

    Add files via upload

--- Pekan 09/075_Binti Nur Hamidah/eksperimen.php
+++ Pekan 09/075_Binti Nur Hamidah/eksperimen.php
@@ -1,17 +1,22 @@
 <?php
 
 echo "<h2>Profil Programmer</h2>";
-echo "<hr><br>";
+echo "<hr>";
 
-echo "Nama : Binti Nur Hamidah<br>";
-echo "NIM : 240631100075<br>";
-echo "Prodi : Pendidikan Informatika<br>";
-echo "Fakultas : Keguruan dan Ilmu Pendidikan<br>";
+echo "<br>";
+
+echo "Nama        : Binti Nur Hamidah<br>";
+echo "NIM         : 240631100075<br>";
+echo "Prodi       : Pendidikan Informatika<br>";
+echo "Fakultas    : Keguruan dan Ilmu Pendidikan<br>";
 echo "Universitas : Universitas Trunojoyo Madura<br>";
-echo "Hobi : Membaca novel dan menonton<br>";
-echo "Cita-cita : Guru<br>";
-echo "Alasan belajar web programming : Ingin lebih menguasai bidang IT<br>";
-echo "Bahasa favorit : PHP<br>";
-echo "Tahun belajar : 2026<br>";
+
+echo "<br><hr><br>";
+
+echo "Hobi        : Membaca novel dan menonton<br>";
+echo "Cita-cita   : Guru<br>";
+echo "Alasan      : Ingin lebih menguasai bidang IT<br>";
+echo "Bahasa      : PHP<br>";
+echo "Tahun       : 2026<br>";
 
 ?>
