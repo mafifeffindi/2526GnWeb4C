ac0e0dca599a889b0f8435414afef5f950d93116
075_Binti Nur Hamidah <bintihamida7@gmail.com>
2026-05-05 07:52:06 +0700

    Add files via upload

---
+++ Pekan 09/099_Naswa Sabrina Irani/halaman.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan PHP</title>
+</head>
+<body>
+
+<h1>Belajar PHP Dasar</h1>
+
+<p>Ini adalah contoh halaman yang menggabungkan HTML dan PHP sederhana.</p>
+
+<?php
+
+echo "Hari ini saya belajar server side programming";
+echo "<br>";
+
+echo "Saya menggunakan bahasa PHP";
+echo "<br>";
+
+echo "Belajar ini membantu saya memahami cara kerja website";
+
+?>
+
+</body>
+</html>
