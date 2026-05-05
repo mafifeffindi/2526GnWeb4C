dd90998ad4a9d5d80909a124922c1732e996cb8c
khomaidi <ahmdkhomaidy@gmail.com>
2026-05-05 07:21:13 +0700

    Create halaman.php

---
+++ Pekan 09/103_Homaidi/halaman.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html> 
+<html lang="id"> 
+<head> 
+    <title>Latihan PHP</title> 
+</head> <body> 
+ 
+<h1>Belajar PHP</h1> 
+ 
+<p>Ini adalah paragraf pertama dalam latihan PHP. Server side programming memungkinkan kita membuat halaman web dinamis.</p> 
+ 
+<?php 
+// Baris output PHP ke-1 echo "Hari ini saya belajar server side programming.<br>"; 
+ 
+// Baris output PHP ke-2 
+echo "PHP adalah bahasa pemrograman yang populer untuk web 
+development.<br>"; 
+ 
+// Baris output PHP ke-3 echo "Saya senang bisa mempelajari PHP lebih dalam."; 
+?> 
+ 
+</body> 
+</html> 
