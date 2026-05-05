1f7372c3cc4da8a09f9a89867f2982451412ac7c
khomaidi <ahmdkhomaidy@gmail.com>
2026-05-05 07:16:49 +0700

    Create eksperimen.php

---
+++ Pekan 09/103_Homaidi/eksperimen.php
@@ -0,0 +1,26 @@
+<?php
+echo "saya belajar PHP";
+echo "<br>";
+echo "hello word";
+echo "<br>";
+echo "Nama : homaidi";
+echo "<br>";
+echo "Umur : ";
+$a = 10;
+$b = 11;
+$hasil = $a + $b;
+echo $hasil;
+echo "<br>";
+echo "Prodi : Pendidikan Informatika";
+echo "<br>";
+echo "Alamat : Sampang";
+echo "<br>";
+$kalimat = "saya belajar PHP";
+echo $kalimat;
+echo "<br";
+echo strlen ($kalimat);
+echo "<br>";
+echo "Motivasi";
+echo "<br>";
+echo "usaha tidak menghinati hasil";
+?>
