ca3b26c838cdd62c6b1dab7ce12d5cde2a6f8014
zahraaaaaa2201-fatimah <zahraaaaaa2201@gmail.com>
2026-05-04 18:28:43 -0700

    Add files via upload

---
+++ Pekan 09/fatimah_101_pekan9/eksperimen.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "<h2>Profil Programmer</h2>";
+echo "<hr>";
+
+echo "Nama : Fatimah";
+echo "<br>";
+
+echo "Universitas : Universitas Trunojoyo Madura";
+echo "<br>";
+
+echo "Hobi : Ngoding dan Gaming";
+echo "<br>";
+
+echo "Cita-cita : Menjadi Web Developer";
+echo "<br>";
+
+echo "Alasan belajar web programming : Ingin membuat website sendiri dan berguna bagi orang lain";
+echo "<br>";
+
+echo "Bahasa favorit : PHP";
+echo "<br>";
+
+echo "Tahun belajar : 2026";
+
+?>
---
+++ Pekan 09/fatimah_101_pekan9/halaman.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan PHP</title>
+</head>
+
+<body>
+
+<h1>Belajar PHP</h1>
+
+<p>Ini adalah halaman latihan dasar PHP untuk memahami server-side programming.</p>
+
+<?php
+
+echo "Hari ini saya belajar server side programming";
+echo "<br>";
+
+echo "Saya menggunakan bahasa PHP";
+echo "<br>";
+
+echo "Saya ingin menjadi web developer";
+
+?>
+
+</body>
+</html>
