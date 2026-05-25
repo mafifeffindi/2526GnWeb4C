2d3a25913fe5439d23c0afea78a9aa0cf718948f
khomaidi <ahmdkhomaidy@gmail.com>
2026-05-25 22:43:39 +0700

    Create tambahan.php

---
+++ pekan 13 /103_homaidi/tambahan.php
@@ -0,0 +1,56 @@
+<?php
+include 'koneksi.php';
+
+if(isset($_POST['submit'])){
+
+    mysqli_query($conn,
+    "INSERT INTO mahasiswa VALUES(
+    '',
+    '$_POST[nama]',
+    '$_POST[nim]',
+    '$_POST[prodi]')");
+
+    header("Location:index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title>Tambah Data</title>
+
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <h2>Tambah Data Mahasiswa</h2>
+
+    <form method="POST">
+
+        <label>Nama</label>
+
+        <input type="text" name="nama">
+
+        <label>NIM</label>
+
+        <input type="text" name="nim">
+
+        <label>Jurusan</label>
+
+        <input type="text" name="prodi">
+
+        <button type="submit" name="submit">
+            Simpan
+        </button>
+
+    </form>
+
+</div>
+
+</body>
+</html>
