134189bcee4cf9c7f02f93348bf630bc1ce37141
mohfarhanyamani106-dev <mohfarhanyamani106@gmail.com>
2026-06-02 08:27:45 +0700

    Update halaman.php

--- Pekan 09/106_Moh. Farhan Yamani/eksperimen/halaman.php
+++ Pekan 09/106_Moh. Farhan Yamani/eksperimen/halaman.php
@@ -1,1 +1,28 @@
+<?php
+// File: eksperimen.php
+// Nama: Moh. Farhan Yamani
+// NIM: 106
+?>
 
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Profil Saya</title>
+</head>
+<body>
+
+    <h1>Profil Mahasiswa</h1>
+    <hr>
+
+    <?php
+        echo "<p><strong>Nama:</strong> Moh. Farhan Yamani</p>";
+        echo "<p><strong>NIM:</strong> 106</p>";
+        echo "<p><strong>Universitas:</strong> Universitas Trunojoyo Madura</p>";
+        echo "<p><strong>Hobi:</strong> Mendengarkan musik dan membaca novel</p>";
+        echo "<p><strong>Cita-cita:</strong> Menjadi Web Developer profesional</p>";
+        echo "<p><strong>Alasan Belajar Web Programming:</strong> Ingin bisa membuat website sendiri dan membuka peluang karir di bidang teknologi</p>";
+    ?>
+
+</body>
+</html>
