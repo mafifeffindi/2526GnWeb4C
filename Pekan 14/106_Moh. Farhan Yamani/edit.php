41beaf38e1d0f86a923c52c315fc7f547ce9821a
mohfarhanyamani106-dev <mohfarhanyamani106@gmail.com>
2026-06-02 08:33:50 +0700

    Update edit.php

--- Pekan 14/106_Moh. Farhan Yamani/edit.php
+++ Pekan 14/106_Moh. Farhan Yamani/edit.php
@@ -1,1 +1,89 @@
+<?php
 
+session_start();
+
+if(!isset($_SESSION['login'])){
+    header("Location: login.php");
+    exit;
+}
+
+include 'koneksi.php';
+
+$id = $_GET['id'];
+
+$data = mysqli_query($koneksi,
+"SELECT * FROM mahasiswa WHERE id='$id'");
+
+$d = mysqli_fetch_array($data);
+
+if(isset($_POST['update'])){
+
+    $nim = $_POST['nim'];
+    $nama = $_POST['nama'];
+    $prodi = $_POST['prodi'];
+    $email = $_POST['email'];
+    $no_hp = $_POST['no_hp'];
+
+    mysqli_query($koneksi,
+    "UPDATE mahasiswa SET
+    nim='$nim',
+    nama='$nama',
+    prodi='$prodi',
+    email='$email',
+    no_hp='$no_hp'
+    WHERE id='$id'");
+
+    header("location:index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Edit Data</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<div class="container">
+
+    <h2>Edit Data Mahasiswa</h2>
+
+    <form method="post">
+
+        <input type="text"
+        name="nim"
+        value="<?php echo $d['nim']; ?>">
+
+        <input type="text"
+        name="nama"
+        value="<?php echo $d['nama']; ?>">
+
+        <input type="text"
+        name="prodi"
+        value="<?php echo $d['prodi']; ?>">
+
+        <input type="text"
+        name="email"
+        value="<?php echo $d['email']; ?>">
+
+        <input type="text"
+        name="no_hp"
+        value="<?php echo $d['no_hp']; ?>">
+
+        <button type="submit" name="update">
+            Update
+        </button>
+
+    </form>
+
+</div>
+
+</body>
+
+</html>
