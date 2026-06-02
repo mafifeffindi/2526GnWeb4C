5a2301757faf0496ff38a58be1dda2f1275f6375
mohfarhanyamani106-dev <mohfarhanyamani106@gmail.com>
2026-06-02 08:35:09 +0700

    Update hapus.php

--- Pekan 14/106_Moh. Farhan Yamani/hapus.php
+++ Pekan 14/106_Moh. Farhan Yamani/hapus.php
@@ -1,1 +1,19 @@
+<?php
 
+session_start();
+
+if(!isset($_SESSION['login'])){
+    header("Location: login.php");
+    exit;
+}
+
+include 'koneksi.php';
+
+$id = $_GET['id'];
+
+mysqli_query($koneksi,
+"DELETE FROM mahasiswa WHERE id='$id'");
+
+header("location:index.php");
+
+?>
