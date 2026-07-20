73da7a580bdee402fb270dcbf2f08f776d4b05dc
nadinecahaya052-ui <nadinecahaya052@gmail.com>
2026-07-20 17:33:15 +0700

    Add files via upload

---
+++ pekan 13 /102_/Nadine Cahaya Oktavia/indeks.php
@@ -0,0 +1,71 @@
+<?php
+
+include 'koneksi.php';
+
+$data = mysqli_query($koneksi, "SELECT * FROM mahasiswa");
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>Data Mahasiswa</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+
+<h2>Data Mahasiswa</h2>
+<a href="tambah.php" class="tambah">
+    + Tambah Data
+</a>
+
+<table border="1" cellpadding="10">
+    <tr>
+        <th>No</th>
+        <th>NIM</th>
+        <th>Nama</th>
+        <th>Prodi</th>
+        <th>Email</th>
+        <th>No HP</th>
+        <th>Aksi</th>
+    </tr>
+
+<?php
+
+$no = 1;
+while($d = mysqli_fetch_array($data)){
+?>
+<tr>
+    <td><?php echo $no++; ?></td>
+    <td><?php echo $d['nim']; ?></td>
+    <td><?php echo $d['nama']; ?></td>
+    <td><?php echo $d['prodi']; ?></td>
+    <td><?php echo $d['email']; ?></td>
+    <td><?php echo $d['no_hp']; ?></td>
+    <td>
+        <a href="edit.php?id=<?php echo $d['id']; ?>" class="edit">
+            Edit
+</a>
+        <a href="hapus.php?id=<?php echo $d['id']; ?>" class="hapus">
+            Hapus
+</a>   
+
+    </td>
+</tr>
+
+<?php
+
+}
+
+?>
+
+</table>
+
+</div>
+
+</body>
+
+</html>
